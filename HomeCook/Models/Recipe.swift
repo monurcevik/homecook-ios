05c8166f8d9aca87ed7cbac6d0aa07fb4c1b125d
Onur Cevik <onur.cevik@metu.edu.tr>
2021-02-23 00:20:31 +0300

    created recipe model - second draft

--- HomeCook/Models/Recipe.swift
+++ HomeCook/Models/Recipe.swift
@@ -4,4 +4,21 @@ struct Recipe: Codable {
     let ingredients: [Ingredient]?
     let cook: User?
     let instructions: String?
+    let duration: Double? // in seconds
+    let difficulty: RecipeDifficulty
+}
+
+enum RecipeDifficulty {
+    case Easy, Meh, Hard
+    
+    var emoji: String {
+        switch self {
+        case .Easy:
+            return "😬"
+        case .Meh:
+            return "🧐"
+        case .Hard:
+            return "😦"
+        }
+    }
 }
